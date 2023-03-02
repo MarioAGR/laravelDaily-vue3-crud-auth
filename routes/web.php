979a635aeb203fd21706ccf2fdc083331da666df
Mario Guillen <marioagr95@gmail.com>
2023-03-01 21:01:00 -0600

    E13 - Laravel Routing Fix: "Catch" All Vue.js Routes

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'dashboard')->name('dashboard');
+Route::view('/{any?}', 'dashboard')
+    ->name('dashboard')
+    ->where('any', '.*');
