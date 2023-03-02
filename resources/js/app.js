99fb9afeb0d27f53e3abe8cdbf8943c26ff4b02f
Mario Guillen <marioagr95@gmail.com>
2023-03-01 21:11:48 -0600

    E14 - Vue.js Routing: Route Names and Separate File

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,9 @@
 import './bootstrap';
 
 import { createApp } from 'vue/dist/vue.esm-bundler';
-import { createRouter, createWebHistory } from "vue-router";
 import App from "./layouts/App.vue";
-import PostsIndex from "./components/Posts/Index.vue";
-import PostsCreate from "./components/Posts/Create.vue";
-
 import { TailwindPagination } from 'laravel-vue-pagination';
-
-const routes = [
-    { path: '/', component: PostsIndex },
-    { path: '/posts/create', component: PostsCreate }
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
+import router from "./routes/index";
 
 const app = createApp(App);
 app.use(router);
